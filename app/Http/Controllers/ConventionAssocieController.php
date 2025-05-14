e44a26814f58840dfac8147c83ca79e4ece46c6b
Joël Kamo <kamostephane@gmail.com>
2025-05-14 10:10:59 +0100

    Ajout de la vérification des conventions actives dans les méthodes store et update du contrôleur ConventionAssocieController. Amélioration de la gestion des exceptions dans ConventionAssocieRequest.

--- app/Http/Controllers/ConventionAssocieController.php
+++ app/Http/Controllers/ConventionAssocieController.php
@@ -10,7 +10,7 @@ class ConventionAssocieController extends Controller
 {
     /**
      * @return JsonResponse
-     * 
+     *
      * @permission ConventionAssocieController::index
      * @permission_desc Afficher la liste des conventions
      */
@@ -21,7 +21,7 @@ class ConventionAssocieController extends Controller
                 'client:id,nomcomplet_client',
                 'createdBy:id,nom_utilisateur',
                 'updatedBy:id,nom_utilisateur'
-            ])->paginate(
+            ])->latest()->paginate(
                 perPage: request('per_page', 15),
                 page: request('page', 1),
             )
@@ -31,12 +31,20 @@ class ConventionAssocieController extends Controller
     /**
      * @param ConventionAssocieRequest $request
      * @return JsonResponse
-     * 
+     *
      * @permission ConventionAssocieController::store
      * @permission_desc Créer une nouvelle convention
      */
     public function store(ConventionAssocieRequest $request)
     {
+        try {
+            $request->checkValidConventionInProgress();
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
+
         ConventionAssocie::create($request->validated());
 
         return response()->json([
@@ -48,12 +56,20 @@ class ConventionAssocieController extends Controller
      * @param ConventionAssocieRequest $request
      * @param ConventionAssocie $conventionAssocie
      * @return JsonResponse
-     * 
+     *
      * @permission ConventionAssocieController::update
      * @permission_desc Mettre à jour une convention
      */
     public function update(ConventionAssocieRequest $request, ConventionAssocie $conventionAssocie)
     {
+        try {
+            $request->checkValidConventionInProgress();
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
+
         $conventionAssocie->update($request->validated());
 
         return response()->json([
@@ -64,12 +80,26 @@ class ConventionAssocieController extends Controller
     /**
      * @param ConventionAssocie $conventionAssocie
      * @return JsonResponse
-     * 
+     *
      * @permission ConventionAssocieController::activate
      * @permission_desc Activer ou désactiver une convention
      */
     public function activate(ConventionAssocie $conventionAssocie)
     {
+        if (! $conventionAssocie->active) {
+            $convention = ConventionAssocie::where('client_id', $conventionAssocie->client_id)
+                ->where('id', '!=', $conventionAssocie->id)
+                ->where('active', true)
+                ->where('end_date', '>', $conventionAssocie->start_date)
+                ->first();
+
+            if ($convention) {
+                return response()->json([
+                    'message' => 'Une convention en cours est deja enregistrée pour ce client associé !'
+                ], 400);
+            }
+        }
+
         $conventionAssocie->update([
             'active' => !$conventionAssocie->active
         ]);
--- app/Http/Requests/ConventionAssocieRequest.php
+++ app/Http/Requests/ConventionAssocieRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Models\ConventionAssocie;
+use Exception;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Log;
 
 class ConventionAssocieRequest extends FormRequest
 {
@@ -15,5 +18,25 @@ class ConventionAssocieRequest extends FormRequest
             'start_date' => ['required', 'date'],
             'end_date' => ['required', 'date'],
         ];
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function checkValidConventionInProgress(): void
+    {
+        $convention = $this->route('convention_associe');
+
+        $convention = ConventionAssocie::where('client_id', $this->client_id)
+            ->when($convention, function ($query) use ($convention) {
+                return $query->where('id', '!=', $convention->id);
+            })
+            ->where('active', true)
+            ->where('end_date', '>', $this->start_date)
+            ->first();
+
+        if ($convention) {
+            throw new Exception('Une convention en cours est deja enregistrée pour ce client associé ', 400);
+        }
     }
 }
