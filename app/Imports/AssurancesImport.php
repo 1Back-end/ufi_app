b26e1d0c702230f7da2a331f0d125629ae4b36d8
joel kamo <kamostephane@gmail.com>
2025-09-08 17:01:29 +0100

    Import Assureurs

--- app/Imports/AssurancesImport.php
+++ app/Imports/AssurancesImport.php
@@ -4,32 +4,17 @@ namespace App\Imports;
 
 use App\Models\Assureur;
 use App\Models\Centre;
-use App\Models\Consultant;
 use App\Models\Quotation;
-use Faker\Factory as Faker;
-use Maatwebsite\Excel\Concerns\ToModel;
+use App\Models\User;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\ToCollection;
+
 class AssurancesImport implements ToCollection
 {
     public function collection(Collection $rows)
     {
-        ini_set('max_execution_time', 300);
-
-        $authId = auth()->id();
-        $faker = Faker::create();
 
         $rows->shift(); // Supprime l'en-tête
-
-        // Tableau pour garder la trace des valeurs uniques déjà rencontrées dans le fichier
-        $emails = [];
-        $tels = [];
-        $tels1 = [];
-        $refs = [];
-        $reg_coms = [];
-        $num_coms = [];
 
         foreach ($rows as $index => $row) {
 
@@ -37,7 +22,7 @@ class AssurancesImport implements ToCollection
             $nom_assur         = trim((string) ($row[2] ?? ''));
             $adresse_assur     = trim((string) ($row[3] ?? ''));
             $Contact_assur     = trim((string) ($row[4] ?? ''));
-            $code_quot         = trim((string) ($row[5] ?? ''));
+            $code_quot         = trim((string) ($row[18] ?? ''));
             $Reg_com_assur     = trim((string) ($row[6] ?? ''));
             $num_contrib_assur = trim((string) ($row[7] ?? ''));
             $bp_assur          = trim((string) ($row[8] ?? ''));
@@ -52,32 +37,20 @@ class AssurancesImport implements ToCollection
                 continue; // Ignore si la quotation n'existe pas
             }
 
-            // Vérifie les doublons dans le fichier
-            if (
-                in_array($Email_assur, $emails) ||
-                in_array($Contact_assur, $tels) ||
-                in_array($Tel_assur, $tels1) ||
-                in_array($ref_assur, $refs) ||
-                in_array($Reg_com_assur, $reg_coms) ||
-                in_array($num_contrib_assur, $num_coms)
-            ) {
-                continue; // Ignore cette ligne si doublon dans le fichier
-            }
-
-            // Ajoute les valeurs dans les tableaux de suivi pour éviter les doublons
-            $emails[] = $Email_assur;
-            $tels[] = $Contact_assur;
-            $tels1[] = $Tel_assur;
-            $refs[] = $ref_assur;
-            $reg_coms[] = $Reg_com_assur;
-            $num_coms[] = $num_contrib_assur;
-
             // Crée ou met à jour l'assureur dans la base
             Assureur::updateOrCreate(
-                ['email' => $Email_assur], // clé pour update
+                [
+                    'email' => $Email_assur,
+                    'tel' => $Contact_assur,
+                    'tel1' => $Tel_assur,
+                    'ref' => $ref_assur,
+                    'Reg_com' => $Reg_com_assur,
+                    'num_com' => $num_contrib_assur,
+                ],
                 [
                     'ref' => $ref_assur,
                     'nom' => $nom_assur,
+                    'email' => $Email_assur,
                     'nom_abrege' => $nom_assur,
                     'adresse' => $adresse_assur,
                     'tel' => $Contact_assur,
@@ -89,6 +62,8 @@ class AssurancesImport implements ToCollection
                     'bp' => $bp_assur,
                     'fax' => $fax_assur,
                     'code_type' => 'Principale',
+                    'created_by' => User::first()->id,
+                    'updated_by' => User::first()->id,
                 ]
             );
         }
