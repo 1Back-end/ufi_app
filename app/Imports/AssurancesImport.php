11654ec65e0e22ec616969bc4824775e0651b3d7
joel kamo <kamostephane@gmail.com>
2025-09-08 18:29:57 +0100

    Assureur

--- app/Imports/AssurancesImport.php
+++ app/Imports/AssurancesImport.php
@@ -7,45 +7,36 @@ use App\Models\Centre;
 use App\Models\Quotation;
 use App\Models\User;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class AssurancesImport implements ToCollection
+class AssurancesImport implements ToCollection, WithHeadingRow
 {
     public function collection(Collection $rows)
     {
-
-        $rows->shift(); // Supprime l'en-tête
-
         foreach ($rows as $index => $row) {
+            $ref_assur         = $row['code_assur'] == 'NULL' || empty($row['code_assur']) ? null : $row['code_assur'];
+            $nom_assur         = $row['nom_assur'] == 'NULL' || empty($row['nom_assur']) ? null : $row['nom_assur'];
+            $adresse_assur     = $row['adresse_assur'] == 'NULL' || empty($row['adresse_assur']) ? 'A MODIFIER' : $row['adresse_assur'];
+            $Contact_assur     = $row['contact_assur'] == 'NULL' || empty($row['contact_assur']) ? null : $row['contact_assur'];
+            $Reg_com_assur     = $row['reg_com_assur'] == 'NULL' || empty($row['reg_com_assur']) ? 'A MODIFIER' : $row['reg_com_assur'];
+            $num_contrib_assur = $row['num_contrib_assur'] == 'NULL' || empty($row['num_contrib_assur']) ? 'A MODIFIER' : $row['num_contrib_assur'];
+            $bp_assur          = $row['bp_assur'] == 'NULL' || empty($row['bp_assur']) ? 'A MODFIER' : $row['bp_assur'];
+            $fax_assur         = $row['fax_assur'] == 'NULL' || empty($row['fax_assur']) ? 'A MODIFIER' : $row['fax_assur'];
+            $Email_assur       = $row['email_assur'] == 'NULL' || empty($row['email_assur']) ? 'assurance' . $index . '@example.com' : $row['email_assur'];
+            $Tel_assur         = empty($row[13]) || $row[13] == 'NULL' ? '' : $row[13];
 
-            $ref_assur         = trim((string) ($row[1] ?? ''));
-            $nom_assur         = trim((string) ($row[2] ?? ''));
-            $adresse_assur     = trim((string) ($row[3] ?? ''));
-            $Contact_assur     = trim((string) ($row[4] ?? ''));
-            $code_quot         = trim((string) ($row[18] ?? ''));
-            $Reg_com_assur     = trim((string) ($row[6] ?? ''));
-            $num_contrib_assur = trim((string) ($row[7] ?? ''));
-            $bp_assur          = trim((string) ($row[8] ?? ''));
-            $fax_assur         = trim((string) ($row[9] ?? ''));
-            $Email_assur       = trim((string) ($row[12] ?? 'assurance'.$index.'@example.com'));
-            $Tel_assur         = trim((string) ($row[13] ?? ''));
-
-            $code_quot = Quotation::find($code_quot);
+            $code_quot = Quotation::find($row['id']);
             $code_centre = Centre::first();
 
             if (!$code_quot) {
                 continue; // Ignore si la quotation n'existe pas
             }
 
-            $assureur = Assureur::where('ref', $ref_assur)
-                ->where('email', $Email_assur)
-                ->where('tel', $Contact_assur)
-                ->where('tel1', $Tel_assur)
-                ->where('Reg_com', $Reg_com_assur)
-                ->where('num_com', $num_contrib_assur)
+            $assureur = Assureur::where('ref', $row['code_assur'])
                 ->first();
 
-            
             if ($assureur) {
                 $assureur->update([
                     'ref' => $ref_assur,
