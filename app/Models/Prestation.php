05ea07b98ff1315fa445c437bdc0ea4cbf98dadb
joel kamo <kamostephane@gmail.com>
2025-10-03 11:24:22 +0100

    UPDATE

--- app/Models/Prestation.php
+++ app/Models/Prestation.php
@@ -242,7 +242,8 @@ class Prestation extends Model
                 $facture = $this->factures()->where('factures.type', 2)->first();
                 if ($facture) {
                     if ($this->payable_by || $this->priseCharge) {
-                        return $facture->created_at->addMinutes(30)->gt(now());
+//                        return $facture->created_at->addMinutes(30)->gt(now());
+                        return true;
                     }
 
                     return $facture->amount_client && $facture->regulations()->where('regulations.state', StatusRegulation::ACTIVE->value)->count() == 0;
