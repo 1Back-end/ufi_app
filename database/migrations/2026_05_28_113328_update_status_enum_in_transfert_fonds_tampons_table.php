42ac74efe3f8aadd353682df7de3b34a6aeee3c5
Laurent Alphonse <laurentalphonsewilfried@gmail.com>
2026-05-28 11:37:54 +0100

    Mise à jour des status sur la validation des transferts

---
+++ database/migrations/2026_05_28_113328_update_status_enum_in_transfert_fonds_tampons_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('transfert_fonds_tampons', function (Blueprint $table) {
+            $table->string('status', 50)->default('pending')->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('transfert_fonds_tampons', function (Blueprint $table) {
+            $table->enum('status', [
+                'pending',
+                'validated',
+                'rejected',
+                'failed'
+            ])->default('pending')->change();
+        });
+    }
+};
